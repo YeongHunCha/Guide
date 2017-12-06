a51017f9d747ca1ac48bb76caa5e62c16a51eafc
YeongHunCha <chyh3272@gmail.com>
2017-12-06 21:19:16 +0900

    Add nav-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { RouterModule, Routes } from '@angular/router'
 
 import { UsersService } from './services/users.service';
 import { AuthService } from './services/auth.service';
@@ -13,17 +14,29 @@ import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
 import { UsersComponent } from './components/users/users.component';
 import { UserFormComponent } from './components/user-form/user-form.component';
+import { NavComponent } from './components/nav/nav.component';
+import { MyInfoComponent } from './components/my-info/my-info.component';
+
+const routes : Routes = [
+  {path : '', component: UsersComponent},
+  {path : 'login', component: UserFormComponent},
+  {path : 'myInfo', component: MyInfoComponent},
+
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
-    UserFormComponent
+    UserFormComponent,
+    NavComponent,
+    MyInfoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
+    RouterModule.forRoot(routes),
     AngularFireModule.initializeApp(environment.firebase, 'guide'),
     AngularFirestoreModule,
     AngularFireAuthModule
